530a4fa81576d37ebedb389030db7337b9ead4fb
idk <idk@idk.local>
2020-03-29 17:45:09 -0400

    add default args and don't go private by default, let the extension and the wrapper do it

--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
-    "log"
-    . "github.com/eyedeekay/go-ccw"
+	. "github.com/eyedeekay/go-ccw"
+	"log"
 )
 
 var EXTENSIONS = []string{"./i2pchrome.js"}
+var ARGS = []string{
+	"--safebrowsing-disable-download-protection",
+	"--disable-client-side-phishing-detection",
+	"--no-startup-window",
+	"--disable-3d-apis",
+	"--disable-accelerated-2d-canvas",
+	"--disable-remote-fonts",
+	"--disable-sync-preferences",
+	"--disable-sync",
+	"--disable-speech",
+	"--disable-webgl",
+	"--disable-reading-from-canvas",
+	"--disable-gpu",
+	"--disable-32-apis",
+	"--disable-auto-reload",
+	"--disable-background-networking",
+	"--disable-d3d11",
+	"--disable-file-system",
+}
 
-var CHROMIUM, ERROR = ExtendedChromium("basic", true, EXTENSIONS, "")
+var CHROMIUM, ERROR = ExtendedChromium("i2pchromium", false, EXTENSIONS, ARGS...)
 
 func main() {
-    if err := Run(); err != nil {
-        log.Fatal(err)
-    }
+	if err := Run(); err != nil {
+		log.Fatal(err)
+	}
 }
